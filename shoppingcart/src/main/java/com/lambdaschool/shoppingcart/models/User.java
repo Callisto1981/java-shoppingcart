663f68a33025d727744c30352d1b535512544f75
Wayne <wabbruscatoorr@gmail.com>
2020-09-16 16:35:46 -0400

    added SecurityUserServiceImpl file and updated User model- get/setPassword

--- shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
+++ shoppingcart/src/main/java/com/lambdaschool/shoppingcart/models/User.java
@@ -25,7 +25,7 @@ public class User
     @Column(nullable = false,
             unique = true)
     private String username;
-
+    private String password;
     private String comments;
 
     @OneToMany(mappedBy = "user",
@@ -57,6 +57,16 @@ public class User
     public void setUsername(String username)
     {
         this.username = username;
+    }
+
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public void setPassword(String password)
+    {
+        this.password = password;
     }
 
     public String getComments()
